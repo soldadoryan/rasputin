d8d2a9691f8823e99d6f4c7bb039eaa978d7fa03
Ryan <ryandrumond@outlook.com.br>
2019-06-05 17:31:26 -0300

    Rasputin não sabe mais o que é...

--- app/Http/Controllers/RedeNeural.php
+++ app/Http/Controllers/RedeNeural.php
@@ -16,17 +16,23 @@ class RedeNeural extends Controller
   CONST VHTAGS = 4;
 
   private $memoriaCognitiva;
+  private $memoriaNeural;
   private $rasputin;
-  private $pergunta_max_pont = 0;
+  private $pergunta_max_pont_mc;
+  private $pergunta_max_pont_mn;
   private $max_pont = 0;
+  private $MNTags = "";
+  private $MNSTags = "";
+  private $MNHTags = "";
 
-  public function __construct(MemoriaCognitiva $memoriaCognitiva, Rasputin $rasputin) {
+  public function __construct(MemoriaCognitiva $memoriaCognitiva, Rasputin $rasputin, MemoriaNeural $memoriaNeural) {
     $this->memoriaCognitiva = $memoriaCognitiva;
+    $this->memoriaNeural = $memoriaNeural;
     $this->rasputin = $rasputin;
   }
 
-
   public function responder($id_personalidade, $pergunta) {
+    //Olá amigão, gostaria de configurar o meu email. Pode me ajudar?
 
     // pega id do rasputin
     $id_rasputin = base64_decode($id_personalidade);
@@ -38,52 +44,96 @@ class RedeNeural extends Controller
 
     // busca dados da memória cognitiva
     $memoria_cognitiva = $this->memoriaCognitiva->where("id_rasputin", $id_rasputin)->get();
+    $memoria_neural = $this->memoriaNeural->where("id_rasputin", $id_rasputin)->get();
 
-    $pontuacao = $this->neuronio($perguntaTratada,$memoria_cognitiva);
+    $pontuacao_mc = $this->neuronio($perguntaTratada,$memoria_cognitiva, "mc");
+    $pontuacao_mn = $this->neuronio($perguntaTratada,$memoria_neural, "mn");
 
-    if($this->max_pont < 4) {
+    $mn = new $this->memoriaNeural;
+    $mn->tags = $this->MNTags;
+    $mn->stags = $this->MNSTags;
+    $mn->htags = $this->MNHTags;
+    $mn->id_resposta =  $this->pergunta_max_pont_mc->id;
+    $mn->id_rasputin =  $id_rasputin;
+
+    $mn->save();
+    //return $pontuacao_mc . " " . $pontuacao_mn;
+    if($pontuacao_mc < 4 && $pontuacao_mn < 4) {
       return "nao entendi, repita por favor";
     }
 
-    $resposta = $this->pergunta_max_pont->resposta;
-
-    return $resposta;
-
+    if($pontuacao_mc > $pontuacao_mn) {
+      return $this->pergunta_max_pont_mc->resposta;
+    } else {
+      if($this->pergunta_max_pont_mn->aprender)
+      return $this->pergunta_max_pont_mn->resposta;
+      else
+      return $this->pergunta_max_pont_mc->resposta;
+    }
   }
 
-  public function neuronio($pergunta,$memoria_cognitiva){
-    foreach ($memoria_cognitiva as $mc) {
+  public function neuronio($pergunta,$memoria_cognitiva,$tipoMemoria){
+    if(count($memoria_cognitiva) > 0) {
+      foreach ($memoria_cognitiva as $mc) {
 
-      $pont = 0;
+        $this->max_pont = 0;
+        $pont = 0;
+        $mn_tags = "";
+        $mn_stags = "";
+        $mn_htags = "";
 
-      for ($i=0; $i < count($pergunta); $i++) {
+        for ($i=0; $i < count($pergunta); $i++) {
+          $Tags = explode(',', $mc->tags);
+          $STags = explode(',', $mc->stags);
+          $HTags = explode(',', $mc->htags);
 
-        $Tags = explode(',', $mc->tags);
-        $STags = explode(',', $mc->stags);
-        $HTags = explode(',', $mc->htags);
+          // dd($mc->htags)
 
-        if(in_array($pergunta[$i],$Tags)){
-          $pont += self::VTAGS;
+          if(in_array($pergunta[$i],$Tags)){
+            $pont += self::VTAGS;
+            if($mn_tags != "")
+            $mn_tags .= "," . $pergunta[$i];
+            else
+            $mn_tags = $pergunta[$i];
+          }
+
+          if(in_array($pergunta[$i],$STags)){
+            $pont += self::VSTAGS;
+            if($mn_stags != "")
+            $mn_stags .= "," . $pergunta[$i];
+            else
+            $mn_stags = $pergunta[$i];
+          }
+
+          print_r($HTags);
+          if(in_array($pergunta[$i],$HTags)){
+            $pont += self::VHTAGS;
+            if($mn_htags != "")
+            $mn_htags .= "," . $pergunta[$i];
+            else
+            $mn_htags = $pergunta[$i];
+          }
         }
 
-        if(in_array($pergunta[$i],$STags)){
-          $pont += self::VSTAGS;
-        }
+        if($this->max_pont <= $pont) {
+          if($tipoMemoria == "mc"){
+            $this->MNTags = $mn_tags;
+            $this->MNSTags = $mn_stags;
+            $this->MNHTags = $mn_htags;
+            $this->pergunta_max_pont_mc = $mc;
+          }
+          else if($tipoMemoria == "mn") {
+            $this->pergunta_max_pont_mn = $mc;
+          }
 
-        if(in_array($pergunta[$i],$HTags)){
-          $pont += self::VHTAGS;
+          $this->max_pont = $pont;
         }
 
       }
 
-      if($this->max_pont < $pont) {
-        $this->pergunta_max_pont = $mc;
-        $this->max_pont = $pont;
-      }
-
+      return $pont;
     }
-
-    return $pont;
+    return 0;
   }
 
   public function tratarPergunta($pergunta) {
@@ -100,6 +150,6 @@ class RedeNeural extends Controller
   }
 
   public function retirarAcentos($pergunta) {
-    return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"), $pergunta);
+    return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/","/(ç)/","/(Ç)/"),explode(" ","a A e E i I o O u U n N c C"), $pergunta);
   }
 }
